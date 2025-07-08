60af316d207375ce51d705cc05af8af5a4860f6d
Đào Ngọc Lễ <ledn.swe@gmail.com>
2025-07-08 20:58:33 +0700

    fix: update image src

--- app/works/[id]/page.tsx
+++ app/works/[id]/page.tsx
@@ -12,7 +12,7 @@ export default async function WorkPage({
       <p className="mt-4">This is the detail page for work {id}.</p>
       <p className="mt-2">You can add more details about this work here.</p>
       <Image
-        src={`https://random.imagecdn.app/500/500?${id}`}
+        src={`https://random.imagecdn.app/500/500`}
         alt={`Work ${id}`}
         width={500}
         height={500}
